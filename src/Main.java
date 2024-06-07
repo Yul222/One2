ac29d93b85d1fc256b05632cf834bfa711adb05a
Yul <montazstroy@inbox.ru>
2024-06-07 10:30:55 +0300

    add scr

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int coast = 56000; // стоимость билета
+        int miles = 20; // рублей для одной бонусной мили
+        System.out.println("количество бонусных миль:");
+        System.out.println((coast / miles) + " рубля ");
+
+    }
+}
